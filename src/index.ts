aebe440261b9ea9e9a65d929e754a9cc759918ea
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-11-19 09:55:37 +0200

    fixed DOM issues by adding types and improving message display

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-// String Types mini-challenge
-// Write a function that will display the most recent reviewers name next to the review total,
-// making sure to assign a type to the parameter, to prevent unwanted behaviour.
-// : string
+// Fixing the Website
 
+// Can you fix my code to show 'Welcome back Bobby' in the Nav Bar for 
+// our couch surfing website? Do this by assigning types to the parameters
+// we pass to our populateUser function, so that we can be aware of
+// errors in our user objects in the future.
+
+const returningUserDisplay = document.querySelector('#returning-user') as HTMLElement
+const userNameDisplay = document.querySelector('#user') as HTMLElement
 const reviewTotalDisplay = document.querySelector('#reviews') as HTMLElement
 
 const reviews = [
@@ -26,9 +30,26 @@ const reviews = [
     },
 ]
 
-// Solution
-function showReviewTotal (value : number, reviewer: string) {
-  reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer
+
+function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
+  const iconDisplay = isLoyalty ? '⭐' : ''
+  reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
 }
 
-showReviewTotal(reviews.length, reviews[0].name)
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+const you = {
+  userName: 'Bobby',
+  isReturning: true,
+}
+
+
+function populateUser(isReturning : boolean, userName : string ) {
+  if (isReturning){
+      returningUserDisplay.innerHTML = 'back'
+  }
+  userNameDisplay.innerHTML = userName
+}
+
+populateUser(you.isReturning, you.userName)
+
